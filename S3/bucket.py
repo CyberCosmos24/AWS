ce1d3f7a82e9150cf36fac45d02802bbd6018ef0
Cosmos <ajaynonu24@gmail.com>
2022-06-11 20:47:23 -0400

    Added Click

--- S3/bucket.py
+++ S3/bucket.py
@@ -1,23 +1,46 @@
 from secrets import access_key, secret_access_key
-
+import click
 import boto3
-import os 
+
 
 
 client =  boto3.client('s3', aws_access_key_id = access_key, aws_secret_access_key = secret_access_key)
-print(access_key)
-print(secret_access_key)
-create = input("Do you want to create a bucket? Yes or No: ")
-if create == "yes":
-    Bucket_value = input("Enter bucket name: ")
-    region_value = input("Enter region name: ")
-    client.create_bucket(Bucket = Bucket_value, Region = region_value)
-elif create == "no":
-    response = client.list_buckets()
-    if response:
+
+@click.group()
+def cli():
+    pass
+
+@click.command()
+def list_bucket():
+    list = client.list_buckets()
+    if list:
         print('Buckets exists..')
-        for bucket in response['Buckets']:
+        for bucket in list['Buckets']:
             print(f'  {bucket["Name"]}')
+@click.command()
+def make_bucket():
+    create = input("Do you want to create a bucket? Yes or No: ")
+    if create == "yes":
+        Bucket_value = input("Enter bucket name: ")
+        region_value = input("Enter region name: ")
+        client.create_bucket(Bucket = Bucket_value, Region = region_value)
+    elif create == "no":
+        return 
+@click.command() 
+def delete_bucket():
+    delete = input("Do you want to delete a bucket? Yes or No: ")
+    if delete == "yes":
+        Bucket_value = input("Enter bucket name: ")
+        client.delete_bucket(Bucket = Bucket_value)
+    elif delete == "no":
+        return 0
+    
+    
+    
+cli.add_command(list_bucket)
+cli.add_command(make_bucket)
+cli.add_command(delete_bucket)
 
-    bucket = input("Enter the S3 bucket you want to use: ") 
-
+if __name__ == '__main__':
+    cli()
+  
